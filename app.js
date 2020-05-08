f5e3d1d2f8db238c3374c285f7c452f0051c61d7
Ariel Khawly <akhawly59@gmail.com>
2020-05-08 13:37:06 -0500

    basic functionality for determining correct password length

--- app.js
+++ app.js
@@ -2,5 +2,24 @@
 let generate = document.querySelector('#generate');
 // 2) add an event listener for the click on the generate button
 generate.addEventListener('click', event => {
-    console.log('clicked!');
+    alert("Let's begin generating your secure password.");
+   let passLength = prompt('Please choose a password length between 8 and 128 characters.')
+    if (8 < passLength && passLength < 128 ) {
+        console.log(`password is ${passLength} chars `);
+    }
+    else if (passLength != Number)  {
+        console.log('please enter a number')
+    }
+    else {
+        console.log('password out of range')
+    }   
+
+
+
+
+   createPassword();
 })
+
+function createPassword() {
+
+}
